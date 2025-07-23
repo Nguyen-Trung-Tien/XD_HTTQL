2a1ee1ebca44f720ac6981b8d6623dfd4fe1010f
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-07-23 17:04:58 +0700

    connect mysql

--- backend/src/service.js
+++ backend/src/service.js
@@ -1,9 +1,19 @@
 // service
 const express = require("express");
 const dotenv = require("dotenv");
+const routes = require("./routers");
+const connectDB = require("./config/connectDB");
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes(app);
+
+connectDB();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
