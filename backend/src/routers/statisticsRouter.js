8f038b0f603068164e8843d290876a2ccc8883d4
PhungVanNhutMinh <2251120429@ut.edu.vn>
2025-08-23 20:28:07 +0700

    router Revenue by period

--- backend/src/routers/statisticsRouter.js
+++ backend/src/routers/statisticsRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const statisticsController = require('../controller/statisticsController');
 
 router.get('/general', statisticsController.getGeneralStats);
+router.get('/revenue-by-period', statisticsController.getRevenueByPeriod);
 
 
 module.exports = router;
